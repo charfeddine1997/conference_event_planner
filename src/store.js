515f14fda29fe33ca49c83c780c272594e307c76
charfeddine1997 <azizcharfeddine1997@gmail.com>
2026-02-06 14:43:13 -0500

    complete react tuto

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import mealsReducer from './mealsSlice';
+import avSlice from './avSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    meals:mealsReducer,
+    av:avSlice
   },
 });
